7cc4c825712504d335b9cb559c2660e15a63163e
shinykiwi <kafountain5@gmail.com>
2024-09-21 20:18:23 -0400

    students pt.1

--- Assets/Scripts/Student.cs
+++ Assets/Scripts/Student.cs
@@ -6,11 +6,20 @@ public class Student : MonoBehaviour
 {
     [SerializeField] private GameObject desk;
     [SerializeField] private GameObject chair;
+    [SerializeField] private GameObject spawn;
+    [SerializeField] private GameObject[] studentOptions;
+
+    private readonly string[] triggers = {"Writing, Seated, Seated2"};
+    private Animator animator;
+
+    private GameObject student;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        student = Instantiate(studentOptions[Random.Range(0, studentOptions.Length - 1)], spawn.transform);
+        animator = student.GetComponent<Animator>();
+        animator.SetTrigger(triggers[Random.Range(0, triggers.Length - 1)]);
     }
 
     // Update is called once per frame
