59ae7618589c7ad624b60a79382ddda4a912ad09
shinykiwi <kafountain5@gmail.com>
2024-09-21 21:17:57 -0400

    random gen students

--- Assets/Scripts/Student.cs
+++ Assets/Scripts/Student.cs
@@ -9,22 +9,24 @@ public class Student : MonoBehaviour
     [SerializeField] private GameObject spawn;
     [SerializeField] private GameObject[] studentOptions;
 
-    private readonly string[] triggers = {"Writing, Seated, Seated2"};
+    private readonly string[] triggers = {"Writing", "Seated", "Seated2", "Seated3"};
     private Animator animator;
 
     private GameObject student;
-    
-    // Start is called before the first frame update
     void Start()
     {
-        student = Instantiate(studentOptions[Random.Range(0, studentOptions.Length - 1)], spawn.transform);
+        student = Instantiate(studentOptions[Random.Range(0, studentOptions.Length)], spawn.transform);
         animator = student.GetComponent<Animator>();
-        animator.SetTrigger(triggers[Random.Range(0, triggers.Length - 1)]);
-    }
+        animator.SetTrigger(triggers[Random.Range(0, triggers.Length)]);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // if the student is a cheater
+        if (student.GetComponentInParent<Cheater>())
+        {
+            animator.SetTrigger("Cheating");
+        }
+        else if (student.GetComponentInParent<Nerd>())
+        {
+            animator.SetTrigger("Nerding");
+        }
     }
 }
